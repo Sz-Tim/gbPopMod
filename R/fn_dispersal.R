44da58121b468d3eb11119a89c5dccc5661a347e
Tim Szewczyk <tim.szewczyk@gmail.com>
2017-11-21 10:45:12 -0700

    moved to aggregated .R

---
+++ R/fn_dispersal.R
@@ -0,0 +1,191 @@
+#' Set short distance dispersal probabilities
+#'
+#' Assign base dispersal probabilities from each cell. Each layer \code{k} in
+#' \code{[1:i, 1:j, k, source.id]} is the SDD neighborhood for cell n. k=1
+#' contains pr(SDD | source.id,i,j); k=2 contains the grid id for each cell in
+#' the neighborhood.
+#' @param ncell Number of inbound grid cells
+#' @param lc.df Dataframe or tibble with xy coords, land cover proportions, and
+#'   cell id info
+#' @param g.p Named list of global parameters
+#' @return Array with dim(disp.rows, disp.cols, 2, ncell) where the third
+#'   dimension contains grid id's for the neighborhood or probabilities to each
+#'   target cell
+#' @keywords sdd, dispersal, probability, probabilities
+#' @export
+
+sdd_set_probs <- function(ncell, lc.df, g.p) {
+  
+  require(purrr); require(tidyverse); require(pbapply); require(fastmatch)
+  
+  # unpack parameters
+  sdd.max <- g.p$sdd.max
+  sdd.rate <- g.p$sdd.rate
+  bird.hab <- g.p$bird.hab
+  
+  # initialize landscape & storage objects
+  n.x <- range(lc.df[,1])
+  n.y <- range(lc.df[,2])
+  nbr <- 2 * sdd.max + 1
+  sdd.i <- array(0, dim=c(nbr, nbr, 2, ncell))
+  bird.hab.ag <- as.matrix(lc.df[,4:9]) %*% (bird.hab %>% divide_by(sum(.)))
+  
+  # generate default dispersal probability matrix
+  d.pr <- matrix(0, nbr, nbr)
+  ctr <- sdd.max + 1  # center index (i=j) for square mx
+  for(i in 1:nbr) {
+    for(j in i:nbr) {
+      d.pr[i,j] <- dexp((i-ctr)^2 + (j-ctr)^2 - 0.5, sdd.rate)
+      if( sqrt((i-ctr)^2 + (j-ctr)^2) > sdd.max ) {
+        d.pr[i,j] <- 0
+      }
+      d.pr[j,i] <- d.pr[i,j]
+    }
+  }
+  d.pr <- d.pr/sum(d.pr)
+  
+  # pair cell IDs for each neighborhood; indexes match neighborhood matrix
+  xx <- map(lc.df$x[lc.df$inbd], ~seq(.-sdd.max, .+sdd.max))
+  yy <- map(lc.df$y[lc.df$inbd], ~seq(.-sdd.max, .+sdd.max))
+  
+  # create lists of on-map xy neighborhood ranges
+  n_ix <- map(xx, ~.[.>=n.x[1] & .<=n.x[2]])
+  n_iy <- map(yy, ~.[.>=n.y[1] & .<=n.y[2]])
+  
+  # generate all xy combinations & neighborhood matrix indices
+  cat("generating neighborhoods...\n")
+  n_i <- map2(n_ix, n_iy, expand_v) 
+  n_x <- map2(xx, n_ix, `%fin%`) %>% map(which) %>% map(range)
+  n_y <- map2(yy, n_iy, `%fin%`) %>% map(which) %>% map(range)
+  
+  # match xy combinations with cell IDs
+  cat("determining neighborhood cell IDs...\n")
+  pboptions(type="none")
+  if(g.p$n_cores > 1) {
+    p.c <- makeCluster(g.p$n_cores)
+    c_i <- pblapply(n_i, function(x) fastmatch::fmatch(x, lc.df$x_y), cl=p.c)
+    stopCluster(p.c)
+  } else {
+    c_i <- pblapply(n_i, function(x) fastmatch::fmatch(x, lc.df$x_y))
+  }
+  
+  cat("calculating probabilities...\n")
+  for(n in 1:ncell) {
+    # find cell ID for each cell in neighborhood
+    sdd.i[n_y[[n]][1]:n_y[[n]][2],
+          n_x[[n]][1]:n_x[[n]][2],2,n] <- matrix(c_i[[n]], 
+                                                 ncol=diff(n_x[[n]])+1,
+                                                 byrow=TRUE)
+    # weight by bird habitat preference
+    ib <- sdd.i[,,2,n] != 0  # inbounds neighbors
+    sdd.i[,,1,n][ib] <- d.pr[ib] * bird.hab.ag[sdd.i[,,2,n][ib]]
+    # set cell ID to 0 if pr(target) == 0 
+    sdd.i[,,2,n][sdd.i[,,1,n]==0] <- 0
+    # progress update
+    if(n %% 5000 == 0) {
+      cat("finished cell", n, "\n")
+    }
+  }
+  cat("finished:", n, "cells\n")
+  sdd.i[,,1,] <- apply(sdd.i[,,1,], 3, function(x) x/sum(x))
+  return(sdd.i)
+}
+
+
+
+
+#' Short distance dispersal
+#'
+#' This function calculates the number of viable seeds deposited within the
+#' short distance dispersal neighborhood of each cell, accounting for the
+#' distance from the source cell, bird habitat preference, the proportion of
+#' fruits eaten by birds, and seed viability post-digestion.
+#' @param id_i Tibble matching cell IDs. \code{id} indexes on the entire grid
+#'   while \code{id_inbd} indexes only inbound cells
+#' @param N.f Tibble of fruits produced in each cell output from
+#'   \code{\link{make_fruits}}
+#' @param pr.eat.ag Vector of proportion of fruits eaten by birds from
+#'   \code{\link{cell_agg}}
+#' @param pr.s.bird Proportion of viable seeds post-digestion
+#' @param sdd.pr Array with dim(i:disp.rows, j:disp.cols, k:2, n:ncell) output
+#'   from \code{\link{sdd_set_probs}}
+#' @param sdd.rate Rate parameter for SDD exponential kernel
+#' @param sdd.st \code{Logical} denoting whether to implement short distance
+#'   dispersal stochastically
+#' @return Tibble with grid id and number of seeds in each cell
+#' @keywords dispersal, SDD
+#' @export
+
+sdd_disperse <- function(id_i, N.f, pr.eat.ag, pr.s.bird, 
+                         sdd.pr, sdd.rate, sdd.st=F) {
+  
+  require(tidyverse); require(magrittr)
+  
+  # calculate seeds deposited within source cell vs emigrants
+  N.source <- N.f %>%
+    mutate(N.produced=(2.3*N.fruit),
+           N.emig=N.produced*(1-pexp(.5,sdd.rate))*pr.eat.ag[id,],
+           N.drop=N.produced-N.emig) %>%
+    mutate(N.emig=N.emig*pr.s.bird,
+           id_inbd=id_i$id_inbd[id])
+  N.seed <- N.source %>% select(id, N.drop) %>% rename(N.dep=N.drop)
+  
+  if(sdd.st) {
+    N.seed$N.dep <- round(N.seed$N.dep)
+    SDD_sd <- unlist(apply(N.source, 1,
+                           function(x) sample(sdd.pr[,,2,x[7]], x[5], 
+                                              replace=TRUE,
+                                              prob=sdd.pr[,,1,x[7]])))
+    SDD_dep <- tabulate(SDD_sd)  # vector of counts for 1:max(SDD_sd)
+    SDD_nonzero <- SDD_dep > 0  # cell id's with N_dep > 0
+    N.seed <- add_row(N.seed, 
+                      id=which(SDD_nonzero), 
+                      N.dep=SDD_dep[SDD_nonzero])
+  } else {
+    # assign emigrants to target cells & sum within each cell
+    N.seed %<>% 
+      add_row(id=apply(N.source, 1, 
+                       function(x) c(sdd.pr[,,2,x[7]])) %>% c, 
+              N.dep=apply(N.source, 1, 
+                          function(x) c(x[5] * sdd.pr[,,1,x[7]])) %>% c) %>%
+      filter(N.dep > 0)
+  }
+  
+  N.seed %<>%
+    group_by(id) %>% 
+    summarise(N=sum(N.dep) %>% round) %>%
+    filter(!is.na(id_i$id_inbd[id]) & N > 0)
+  
+  return(N.seed)
+}
+
+
+
+
+#' Long distance dispersal
+#'
+#' This function assigns n.ldd random long distance dispersal events across the
+#' landcape. A single seed is added to each target cell
+#' @param ncell Number of inbound grid cells
+#' @param id_i Tibble matching cell IDs. \code{id} indexes on the entire grid
+#'   while \code{id_inbd} indexes only inbound cells
+#' @param N.seed \code{N.seed} output from \code{\link{sdd_disperse}} with grid
+#'   id and number of seeds in each cell
+#' @param n.ldd Number of long distance dispersal events per time step
+#' @return Tibble with grid id and number of seeds
+#' @keywords LDD, dispersal
+#' @export
+
+
+ldd_disperse <- function(ncell, id_i, N.seed, n.ldd) {
+  
+  require(tidyverse); require(magrittr)
+  
+  ldd.id <- id_i$id[which(id_i$id_inbd == sample(1:ncell, n.ldd, replace=T))]
+  
+  N.seed %<>% 
+    add_row(id=ldd.id, N=rep(1, n.ldd)) %>%
+    group_by(id) %>% summarise(N=sum(N))
+  
+  return(N.seed)
+}
