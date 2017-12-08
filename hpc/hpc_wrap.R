d45323ceeabfd2b712934fccf4ccdb0c6351b567
Tim Szewczyk <tim.szewczyk@gmail.com>
2017-12-08 08:37:44 -0700

    cleaning up library calls

--- hpc/hpc_wrap.R
+++ hpc/hpc_wrap.R
@@ -2,23 +2,28 @@
 # The authorization key for the private repo is stored in the
 # working directory in git_gb_token.R
 
-# load libraries
+# load most recent version of package on GitHub
 # source("hpc/git_gb_token.R")
 # devtools::install_github("Sz-Tim/gbPopMod", auth_token=git_gb_token)
-library(gbPopMod); library(tidyverse); library(magrittr); library(stringr)
-library(here); library(gganimate); library(fastmatch)
-library(doSNOW); library(scales); theme_set(theme_bw())
+
+# load libraries
+Packages <- c("gbPopMod", "tidyverse", "magrittr", "stringr", "here", "doSNOW",
+              "fastmatch", "scales", "gganimate")
+suppressMessages(invisible(lapply(Packages, library, character.only=TRUE)))
+theme_set(theme_bw())
 data(lc.rct)
 
 
 # set parameters
-n.sim <- 2
-g.p <- set_g_p(tmax=5, lc.r=30, lc.c=30, n.ldd=1, n.cores=2)
+n.sim <- 3
+g.p <- set_g_p(tmax=5, lc.r=30, lc.c=30, n.ldd=1, n.cores=3)
 control.p <- set_control_p()
-p <- c("pr.sb", "pr.s.bird", "pr.s")
+p <- c("pr.sb", "pr.s.bird", "pr.s", "pr.f", "fec")
 p.seq <- list(seq(0.1, 0.8, length.out=3),
               seq(0.3, 0.8, length.out=3),
-              expand_cnpy(Op=c(0.2, 0.9), Cl=c(0.2, 0.9), length_out=2))
+              expand_cnpy(Op=c(0.2, 0.9), Cl=c(0.2, 0.9), length_out=2),
+              expand_cnpy(Op=c(0.2, 0.9), Cl=c(0.2, 0.9), length_out=2),
+              expand_cnpy(Op=c(100, 200), Cl=c(10, 40), length_out=2))
 
 # land cover
 lc.df <- lc.rct %>% 
