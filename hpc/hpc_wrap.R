df5d0a6f4f3e51d2089bb5831a26b45aadd884f4
Sz-Tim <tim.szewczyk@gmail.com>
2018-07-18 11:55:52 -0600

    updated for GSA, emulation

--- hpc/hpc_wrap.R
+++ hpc/hpc_wrap.R
@@ -12,19 +12,17 @@ Packages <- c("gbPopMod", "tidyverse", "magrittr", "stringr", "here", "doSNOW",
 suppressMessages(invisible(lapply(Packages, library, character.only=TRUE)))
 theme_set(theme_bw())
 
-# load files
+# set landscape file
 lc_f <- "data/9km_car.csv"
-par_i <- read.csv("data/param_ranges.csv", stringsAsFactors=F)
 
 # set parameters
-g.p <- set_g_p(tmax=50, lc.r=100, lc.c=100, n.cores=3, sdd.max=2, sdd.rate=8, N.p.t0=30)
-pars <- par_i$p[c(11,12,16:18)]
-nSamp <- 900
+g.p <- set_g_p(tmax=50, lc.r=100, lc.c=100, n.cores=4, 
+               m=c(3,3,7,7,7,7), sdd.max=5, sdd.rate=1, N.p.t0=40)
+par.ls <- set_sensitivity_pars(names(g.p)[10:25])
+nSamp <- 300
 
-# munge
-pars.rng <- filter(par_i, p %in% pars)
-pars.rng <- pars.rng[match(pars, pars.rng$p), ]
-lc.df <- make_grid(lc_f, x.="lon", y.="lat", 4:9) %>% 
+# load landscape
+lc.df <- read_csv(lc_f) %>% 
   filter(y >= (max(.$y) - g.p$lc.r) & x <= g.p$lc.c) %>%
   mutate(id=row_number(), 
          id.in=min_rank(na_if(inbd*id, 0)))
@@ -36,9 +34,21 @@ sdd.pr <- sdd_set_probs(ncell, lc.df, g.p)
 N.init <- pop_init(ngrid, g.p, lc.df)
 
 # run sensitivity analysis
-out <- global_sensitivity(pars, pars.rng, nSamp, ngrid, ncell, g.p, lc.df, 
+out <- global_sensitivity(par.ls, nSamp, ngrid, ncell, g.p, lc.df, 
                           sdd.pr, N.init, control.p=NULL, verbose=T)
+
+nMetric <- 8
+nPar <- ncol(out$results)-nMetric
+par.brt <- vector("list", nMetric)
+for(i in 1:nMetric) {
+  metric <- names(out$results)[nPar+i]
+  names(par.brt)[[i]] <- metric
+  brt.out <- emulate_sensitivity(out$results, par.ls, response=metric)
+  par.brt[[i]] <- brt.out$brt.par
+}
+par.brt <- bind_rows(par.brt, .id="metric")
 
 # store output
 write_csv(out$results, "out/sensitivity_results.csv")
-saveRDS(out, "out/sensitivity_out.rds")
+write_csv(par.brt, "out/BRT_by_param.csv")
+#saveRDS(out, "out/sensitivity_out.rds")
