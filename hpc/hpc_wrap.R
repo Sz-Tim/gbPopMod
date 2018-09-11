930144b4a8b24a45f7b636d9674d1a059bd0b0ff
Sz-Tim <tim.szewczyk@gmail.com>
2018-09-10 20:11:01 -0400

    adjusting GSA parameters

--- hpc/hpc_wrap.R
+++ hpc/hpc_wrap.R
@@ -17,9 +17,9 @@ res <- c("20ac", "9km2")[2]
 par_span <- c("total", "gb")[2]
 
 # set parameters
-g.p <- set_g_p(tmax=20, lc.r=Inf, lc.c=Inf, n.cores=4, N.p.t0=4)
-par.ls <- set_sensitivity_pars(names(g.p)[10:26][-(6:7)], par_span, res)
-nSamp <- 400
+g.p <- set_g_p(tmax=50, lc.r=Inf, lc.c=Inf, n.cores=8, N.p.t0=4)
+par.ls <- set_sensitivity_pars(names(g.p)[10:26][-9], par_span, res)
+nSamp <- 10000
 
 # load landscape
 load(paste0("data/USDA_", res, ".rda"))
