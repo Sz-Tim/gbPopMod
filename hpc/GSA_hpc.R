a727498be9d17ae1a8021ac55594bf54b153f144
Sz-Tim <tim.szewczyk@gmail.com>
2018-09-18 15:19:48 -0600

    renaming for clarification

--- hpc/GSA_hpc.R
+++ hpc/GSA_hpc.R
@@ -17,9 +17,11 @@ res <- c("20ac", "9km2")[2]
 par_span <- c("total", "gb")[2]
 
 # set parameters
-g.p <- set_g_p(tmax=50, lc.r=Inf, lc.c=Inf, n.cores=8, N.p.t0=4)
+g.p <- set_g_p(tmax=50, lc.r=Inf, lc.c=Inf, n.cores=8, N.p.t0=1)
 par.ls <- set_sensitivity_pars(names(g.p)[10:26][-9], par_span, res)
-nSamp <- 10000
+par.ls$N.0 <- list(param="N.0", type="int", LC=1, min=rep(1,6), max=par.ls$K$max)
+g.p$N.0 <- round(g.p$K/2)
+nSamp <- 100
 
 # load landscape
 load(paste0("data/USDA_", res, ".rda"))
@@ -31,11 +33,14 @@ ngrid <- nrow(lc.df)
 ncell <- sum(lc.df$inbd)
 
 # initialize
-N.init <- pop_init(ngrid, g.p, lc.df)
+coord.init <- c(739235.9, 4753487) # 1922: first record; herbarium_records.R
+cell_side <- mean(diff(sort(unique(lc.df$lon))))
+cell.init <- lc.df$id[which(abs(lc.df$lon-coord.init[1]) < cell_side/2 & 
+                              abs(lc.df$lat-coord.init[2]) < cell_side/2)]
 
 # run sensitivity analysis
 out <- global_sensitivity(par.ls, nSamp, ngrid, ncell, g.p, lc.df, 
-                          sdd=NULL, N.init, control.p=NULL, verbose=T, 
+                          sdd=NULL, cell.init, control.p=NULL, verbose=T, 
                           sim.dir=paste0("out/", res, "/", par_span, "/sims/"))
 write_csv(out, paste0("out/", res, "/", par_span, "/gsa_results.csv"))
 
