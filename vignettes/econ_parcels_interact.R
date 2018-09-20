e89d033377943df3b84d270e86fc93ec702aa262
Sz-Tim <tim.szewczyk@gmail.com>
2018-09-20 09:59:30 -0600

    replaced fake regressions with more realistic WTP calculations; no ground cover treatments

--- vignettes/econ_parcels_interact.R
+++ vignettes/econ_parcels_interact.R
@@ -29,6 +29,7 @@ set.seed(1)
 
 
 
+
 # Here, the economic model is written in R for illustration and the buckthorn
 # population is iterated each time step. The basic flow is:
 # 1. Set up and initialization. Store initial buckthorn population, etc
@@ -66,13 +67,16 @@ gifs <- TRUE
 res <- "9km2"  # 9km2 or 20ac
 load(paste0("data/USDA_", res, ".rda"))  # loads dataframe as lc.df
 parcel.df <- read.csv(paste0("data/USDA_", res, "_parcels.csv")) %>% 
-  filter(inbd) %>% droplevels %>%
-  mutate(id.pp=row_number())
+  filter(inbd) %>% droplevels %>% mutate(id.pp=row_number())
 ngrid <- nrow(lc.df)  # number of cells in bounding box
 ncell <- sum(lc.df$inbd)  # number of inbound cells
 npp <- nrow(parcel.df)  # number of pixel-parcels
 LCs <- names(lc.df)[4:9]
 pp.ls <- map(1:ncell, ~which(parcel.df$id.in==.))
+coord.init <- c(739235.9, 4753487) # 1922: first record; herbarium_records.R
+cell_side <- mean(diff(sort(unique(lc.df$lon))))
+cell.init <- lc.df$id[which(abs(lc.df$lon-coord.init[1]) < cell_side/2 & 
+                              abs(lc.df$lat-coord.init[2]) < cell_side/2)]
 # Note that this works with three sets of indexes:
 # - id: rectangular grid ID; identified for all cells
 # - id.in: inbound ID; identified for inbound cells, NA if out of bounds
@@ -83,7 +87,7 @@ pp.ls <- map(1:ncell, ~which(parcel.df$id.in==.))
 
 
 #--- set parameters
-tmax <- 150
+tmax <- 100
 # global parameters: ?set_g_p
 g.p <- set_g_p(tmax=tmax, n.cores=1,  
                K=c(5223180, 0, 770790, 770790, 770790, 770790),
@@ -92,14 +96,26 @@ g.p <- set_g_p(tmax=tmax, n.cores=1,
 
 
 #--- FAKE ECONOMIC DECISION REGRESSIONS
-# pr(treat) = antilogit(b1 + b2*log(N_buckthorn_adults))
-mech_chem_b <- c(-2, 0.5)
-grd_cover_b <- c(-4, 0.6)
-N_seq <- exp(seq(log(1), log(max(g.p$K)), length.out=100))
-plot(log(N_seq), antilogit(cbind(1,log(N_seq)) %*% mech_chem_b), 
-     ylim=c(0,1), type="l", ylab="Pr(manual treatment)", xlab="log(buckthorn)")
-plot(log(N_seq), antilogit(cbind(1,log(N_seq)) %*% grd_cover_b), 
-     ylim=c(0,1), type="l", ylab="Pr(ground cover)", xlab="log(buckthorn)")
+get.actions2 <- function(N_i, K_i, PP_R, PP_T, TRT, N_EFF, T_EFF, 
+                         W, CHEM, B, COST){ 
+  
+  WTP <- B[1] * W * PP_R + 
+    B[2] * ((1-N_i*N_EFF/K_i)/(1-N_i/K_i)-1) * PP_R + 
+    B[3] * CHEM * PP_R + 
+    B[4] * N_i * T_EFF * PP_T
+  
+  return(TRT[which.max(WTP - COST)])
+}
+
+trt.i <- list(trt=c("N", "M", "C", "B"), # treatment name
+              N_eff=c(0, 0.2, 0.3, 0.9), # effectiveness = mortality rate
+              biomass_eff=c(0, 0.1, 0.125, 0.2), # effect on biomass
+              cost=c(0, 200, 100, 300), # cost
+              chem=c(0, 0, 1, 1), # chemical used?
+              wild_eff=c(0, 1, 1, 1)) # effect on wildlife
+WTP_b <- c(10, 40, -20, 60) # willingness-to-pay slopes
+actions <- array(NA, dim=c(npp, tmax+1)) # store actions in each time step
+
 
 
 
@@ -107,12 +123,12 @@ plot(log(N_seq), antilogit(cbind(1,log(N_seq)) %*% grd_cover_b),
 sdd <- sdd_set_probs(ncell, lc.df, g.p, verbose=T)
 B <- matrix(0, nrow=ngrid, ncol=tmax+1) # [cell, year]
 N <- array(0, dim=c(npp, tmax+1, 6, max(g.p$m))) # [px-parcel, year, LC, age]
-N.0.px <- pop_init(ncell, g.p, lc.df, p.0=2000) # [cell, LC, age]
+N.0.px <- pop_init(ngrid, g.p, lc.df, p.0=cell.init)[lc.df$inbd,,]
 N[,1,,] <- round(N.0.px[parcel.df$id.in,,]*parcel.df$Grid_Proportion)
 for(l in 1:6) { if(g.p$m[l] < 7) { N[,,l,g.p$m[l]:(max(g.p$m)-1)] <- NA } }
   
 parcel.df$K_pp <- (as.matrix(parcel.df[,LCs]) %*% g.p$K) %>%
-  multiply_by(parcel.df$Grid_Proportion) %>% round
+  multiply_by(parcel.df$Grid_Proportion) %>% c %>% round
 
 
 
@@ -123,35 +139,35 @@ for(k in 1:g.p$tmax) {
   ##---
   ## 2. Management plans are decided
   ##---
-  # economic decision model
-  #   which cells perform which management actions? Here, management is 
-  #   probabilistic based on adult buckthorn abundance and the made up slopes
-  #   shown above. The two treatment types are generated independently, though
-  #   I believe in your model they'll be considered jointly.
-  N.mx <- cbind(1, log(rowSums(N[,k,,7]) + 0.0001))
-  mech_chem_id.pp <- which(rbinom(npp, 1, antilogit(N.mx %*% mech_chem_b))==1)
-  grd_cover_id.pp <- which(rbinom(npp, 1, antilogit(N.mx %*% grd_cover_b))==1)
+  # Economic decision model: which cells perform which management actions? 
+  N.k <- rowSums(N[,k,,7])  # buckthorn adult abundance
+  presence_pp <- which(N.k>0)  # id.pp with adult buckthorn
+  action_pp <- sapply(presence_pp,
+                      function(x) get.actions2(N_i=N.k[x],
+                                               K_i=parcel.df$K_pp[x],
+                                               PP_R=parcel.df$Wilderness_PP[x],
+                                               PP_T=parcel.df$Timber_PP[x],
+                                               TRT=trt.i$trt,
+                                               N_EFF=trt.i$N_eff,
+                                               T_EFF=trt.i$biomass_eff,
+                                               W=trt.i$wild_eff,
+                                               CHEM=trt.i$chem,
+                                               B=WTP_b,
+                                               COST=trt.i$cost))
+  actions[presence_pp,k] <- action_pp # store pixel-parcel decisions
+  mech_chem_id.pp <- presence_pp[action_pp != "N"] # id.pp that are treating
   
   # Set control parameters, identify treated pixel-parcels
-  #   Here, there are two manual treatments (mechanical, mechanical+chemical) 
-  #   and one ground treatment (cover crop = turf). Any additional treatments 
-  #   listed used in mech_chem.i or grd_cover.i just need to be established 
-  #   first via set_control_p(man.trt=c(MC=0.8, ...), grd.trt=c(Cov=0.005, ...))
   control_par <- set_control_p(null_ctrl=F,
                                man.i=mech_chem_id.pp, # mech/chem parcels
-                               man.trt=c(MC=0.9, M=0.3), # mech/chem effectiveness
-                               grd.i=grd_cover_id.pp, # ground cover parcels
-                               grd.trt=c(Cov=0.005)) # ground cover effectiveness
+                               man.trt=setNames(trt.i$N_eff, trt.i$trt)) 
   # Specify which parcels use which treatments
   if(length(mech_chem_id.pp)>0) {
     mech_chem.i <- data.frame(id=mech_chem_id.pp,
-                              Trt=sample(c("MC", "M"), length(mech_chem_id.pp),
-                                         replace=T)) # random mech vs mech+chem
-  } else {mech_chem.i <- NULL}
-  if(length(grd_cover_id.pp)>0) {
-    grd_cover.i <- data.frame(id=grd_cover_id.pp,
-                              Trt="Cov") # sets Trt="Cov" for all rows
-  } else {grd_cover.i <- NULL}
+                              Trt=action_pp[action_pp != "N"]) 
+  } else {
+    mech_chem.i <- NULL
+  }
   
   
   ##---
@@ -166,7 +182,7 @@ for(k in 1:g.p$tmax) {
   # 4. Seeds germinate and establish
   g.p$n.ldd <- ifelse(k %% 2 == 0, 1, 0)
   out <- iterate_pop_econ(parcel.df, pp.ls, N[,k,,], B[,k], g.p, lc.df, 
-                          sdd, control_par, grd_cover.i, mech_chem.i)
+                          sdd, control_par, grd_cover.i=NULL, mech_chem.i)
   N.1g0 <- which(rowSums(out$N[,1,])>0)
   N[N.1g0,k+1,,] <- out$N[N.1g0,,]
   B[,k+1] <- out$B
@@ -190,19 +206,30 @@ out.all <- left_join(lc.df, N.df, by="id.in") %>%
   gather(year, N, (ncol(lc.df)+1):ncol(.)) %>% 
   mutate(year=as.numeric(year),
          B=c(B))
+actions.ls <- setNames(vector("list", 4), trt.i$trt)
+for(i in 1:length(trt.i$trt)) {
+  actions.ls[[i]] <- map_dfr(setNames(1:ncell, 1:ncell), 
+                          ~colSums(actions[pp.ls[[.]],]==trt.i$trt[i], na.rm=T)/length(pp.ls[[.]])) %>%
+    mutate(year=1:(g.p$tmax+1),
+           action=trt.i$trt[i]) %>%
+    gather(id.in, prop, 1:ncell) %>%
+    mutate(id.in=as.numeric(id.in))
+}
+out.actions <- full_join(filter(lc.df, inbd), 
+                         do.call("rbind", actions.ls), by="id.in")
 out.df <- lc.df %>%
   mutate(N.0=out.all$N[out.all$year==1],
          B.0=B[,1],
          N.final=out.all$N[out.all$year==g.p$tmax+1],
          B.final=B[,g.p$tmax+1])
 
-# final maps
+# final abundance maps
 final.p <- ggplot(out.df, aes(lon, lat))
 final.p + geom_tile(aes(fill=log(N.final))) + scale_fill_viridis(option="B")
 final.p + geom_tile(aes(fill=log(B.final))) + scale_fill_viridis(option="B") 
 
 # abundance through time
-ggplot(out.all, aes(year, N, group=id)) + geom_line(alpha=0.5)
+ggplot(out.all, aes(year, log(N+1), group=id)) + geom_line(alpha=0.5)
 
 # gifs
 if(gifs) {
@@ -214,7 +241,7 @@ if(gifs) {
                       transition_time(year) +  
                       ggtitle("Adult abundance. Year {frame_time}"),
                     nframes=n_distinct(out.all$year), 
-                    width=800, height=600, units="px", fps=5))
+                    width=800, height=600, units="px", fps=10))
   anim_save(paste0(plot.dir, "Seed_abundance.gif"),
             animate(ggplot(out.all, aes(lon, lat)) + 
                       geom_tile(aes(fill=log(B))) +
@@ -222,7 +249,17 @@ if(gifs) {
                       transition_time(year) +  
                       ggtitle("Seed abundance. Year {frame_time}"),
                     nframes=n_distinct(out.all$year), 
-                    width=800, height=600, units="px", fps=5))
+                    width=800, height=600, units="px", fps=10))
+  anim_save(paste0(plot.dir, "Parcel_decisions.gif"),
+            animate(ggplot(out.actions, aes(lon, lat)) + 
+                      geom_tile(aes(fill=prop)) +
+                      scale_fill_viridis(name="Proportion of\npixel-parcels", 
+                                         option="B") + 
+                      transition_time(year) +
+                      facet_wrap(~action) +
+                      ggtitle("Management decisions. Year {frame_time}"),
+                    nframes=n_distinct(out.all$year), 
+                    width=1100, height=1000, units="px", fps=10))
 }
 
 
