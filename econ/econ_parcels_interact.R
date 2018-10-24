17a99c89d7e1e432de7a7a8b7870b4fdf951bb6e
Sz-Tim <tim.szewczyk@gmail.com>
2018-10-24 11:34:32 -0600

    Forest regrowth, differential management in regrowth

--- econ/econ_parcels_interact.R
+++ econ/econ_parcels_interact.R
@@ -69,16 +69,22 @@ tmax <- 50
 Packages <- c("here", "gbPopMod", "tidyverse", "magrittr", "viridis")
 suppressMessages(invisible(lapply(Packages, library, character.only=TRUE)))
 load(paste0("data/USDA_", res, ".rda"))  # loads dataframe as lc.df
+lc.df$Rgrw <- 0
+lc.df <- lc.df[,c(1:4,15,5:14)]
 parcel.df <- read.csv(paste0("data/USDA_", res, "_parcels.csv")) %>% 
-  filter(inbd) %>% droplevels %>% mutate(id.pp=row_number())
-N_0 <- readRDS(paste0("data/inits/N_2018_", res, ".rds"))
+  filter(inbd) %>% droplevels %>% mutate(id.pp=row_number(), Rgrw=0)
+parcel.df <- parcel.df[c(1:11,23,12:22)]
+N_init <- readRDS(paste0("data/inits/N_2018_", res, ".rds"))
+N_0 <- array(0, dim=dim(N_init)+c(0,1,0))
+N_0[,c(1,3:7),] <- N_init
+rm(N_init)
 B_0 <- readRDS(paste0("data/inits/B_2018_", res, ".rds"))
 sdd <- readRDS(paste0("data/inits/sdd_", res, ".rds"))
 sdd.ji <- readRDS(paste0("data/inits/sdd_ji_", res, ".rds"))
 ngrid <- nrow(lc.df)  # number of cells in bounding box
 ncell <- sum(lc.df$inbd)  # number of inbound cells
 npp <- nrow(parcel.df)  # number of pixel-parcels
-LCs <- names(lc.df)[4:9]
+LCs <- names(lc.df)[4:10]
 pp.ls <- map(1:ncell, ~which(parcel.df$id.in==.))
 
 
@@ -91,9 +97,18 @@ if(res=="9km2") {
   g.p$sdd.max <- 7
   g.p$sdd.rate <- 1.4
 }
-parcel.df$K_pp <- (as.matrix(parcel.df[,LCs]) %*% g.p$K) %>%
-  multiply_by(parcel.df$Grid_Proportion) %>% c %>% ceiling
+# adjust parameters for Regrow category: same parameters as Open
+# Categories: 1=Opn, 2=Rgrw, 3=Oth, 4=Dec, 5=Evg, 6=WP, 7=Mxd
+g.p$n.lc <- 7
+lc.par <- which(map_lgl(g.p, ~length(.)>1))
+g.p[lc.par] <- map(g.p[lc.par], ~.[c(1,1:6)])
+for(l in 1:g.p$n.lc) {
+  parcel.df[[paste0("K_", LCs[l])]] <- ceiling(parcel.df[[LCs[l]]] * g.p$K[l] *
+                                                 parcel.df$Grid_Proportion)
+}
+parcel.df$K_pp <- rowSums(select(parcel.df, contains("K_")))
 parcel.df_orig <- parcel.df  # store original proportions if forest regrowth 
+
 
 
 #--- FAKE ECONOMIC & TIMBER HARVEST REGRESSIONS
@@ -112,7 +127,7 @@ time_to_forest <- function(T.0, beta, K, N.t0, N.t1) {
 
 
 #--- FAKE FOREST HARVEST DECISION PARAMETERS
-nharvest <- 500  # number of pixel-parcels that harvest in a given year
+nharvest <- 1000  # number of pixel-parcels that harvest in a given year
 beta <- 1
 L.0 <- 10
 forest.pp <- which(names(parcel.df) %in% c("Dec", "Evg", "WP", "Mxd"))
@@ -128,14 +143,16 @@ trt.i <- list(trt=c("N", "M", "C", "B"), # treatment name
               chem=c(1, 1, -1, -1), # chemical used? (-1 = YES, 1 = No)
               wild_eff=c(0, 1, 1, 1)) # effect on wildlife
 WTP_b <- c(10, 40, -20, 60) # willingness-to-pay slopes
-act.mx <- array(NA, dim=c(npp, tmax)) # store actions in each time step
+Mgmt1_LC <- LCs[c(1,4:7)]  # Opn, Dec, Evg, WP, Mxd
+Mgmt2_LC <- LCs[2]  # Rgrw
+act1.mx <- act2.mx <- array("N", dim=c(npp, tmax)) # store actions in each year
 
 
 
 #--- initialize SDD neighborhoods & buckthorn populations (just load eventually)
 B <- matrix(0, nrow=ngrid, ncol=tmax+1) # [cell, year]
 B[,1] <- B_0
-N <- array(0, dim=c(npp, tmax+1, 6, max(g.p$m))) # [px-parcel, year, LC, age]
+N <- array(0, dim=c(npp, tmax+1, 7, max(g.p$m))) # [px-parcel, year, LC, age]
 N.0.px <- N_0[lc.df$inbd,,]
 N[,1,,] <- round(N.0.px[parcel.df$id.in,,]*parcel.df$Grid_Proportion)
 N[,1,,7] <- pmin(N[,1,,7], as.matrix(parcel.df$K_pp * parcel.df[,LCs]))
@@ -160,36 +177,49 @@ for(k in 1:g.p$tmax) {
   ##    each harvested pixel-parcel. 
   
   # harvest: id.pp, proportion cleared for each forest type (assuming 100% here)
+  # NOTE: K and N are both just for within landcover category 'Rgrw': N[,,2,]
   if(k>1 && nrow(cut.df)>0) {
     cut.df$T_regrow <- time_to_forest(T.0=cut.df$T_regrow,
                                       beta=beta,
-                                      K=parcel.df$K_pp[cut.df$id.pp],
-                                      N.t0=rowSums(N[cut.df$id.pp,k-1,,7]),
-                                      N.t1=rowSums(N[cut.df$id.pp,k,,7]))
+                                      K=parcel.df$K_Rgrw[cut.df$id.pp],
+                                      N.t0=N[cut.df$id.pp,k-1,2,7],
+                                      N.t1=N[cut.df$id.pp,k,2,7])
+    cut_pool <- (1:npp)[-cut.df$id.pp]
+  } else {
+    cut_pool <- 1:npp
   }
-  cut_k <- data.frame(id.pp=sample(1:npp, nharvest),
+  cut_k <- data.frame(id.pp=sample(cut_pool, nharvest),
                       Dec=rep(1, nharvest),
                       Evg=rep(1, nharvest),
                       WP=rep(1, nharvest),
                       Mxd=rep(1, nharvest))
-  cut.df <- bind_rows(cut.df,
-                      data.frame(id.pp=cut_k$id.pp,
-                                 T_regrow=(1+beta*rowSums(N[cut_k$id.pp,k,,7])/
-                                             parcel.df$K_pp[cut_k$id.pp])*L.0))
+  # adjust N: forest becomes Rgrw
+  N_cut <- N[cut_k$id.pp,k,4:7,]
+  for(i in 1:7) { # iterate across ages; multiply N by % cut
+    N_cut[,,i] <- N_cut[,,i] * as.matrix(cut_k[,-1])
+  }
+  N[cut_k$id.pp,k,2,] <- apply(N_cut, c(1,3), sum)
+  N[cut_k$id.pp,k,4:7,] <- N[cut_k$id.pp,k,4:7,] - N_cut
   # regrowth
-  if(any(cut.df$T_regrow < 0)) {
-    regrow_id.pp <- cut.df$id.pp[cut.df$T_regrow < 0]
+  if(any(cut.df$T_regrow <= 0)) {
+    regrow_id.pp <- cut.df$id.pp[cut.df$T_regrow <= 0]
     cut.df <- filter(cut.df, T_regrow > 0)
     # regrow to original proportions
     parcel.df[regrow_id.pp, LCs] <- parcel.df_orig[regrow_id.pp, LCs]
     regrow_id <- sort(unique(parcel.df$id[regrow_id.pp]))
+    # adjust N: Rgrw becomes forest
+    N_regrow <- N[regrow_id.pp,k,2,]
+    for(i in 4:7) {
+      N[regrow_id.pp,k,i,] <- N_regrow * parcel.df[regrow_id.pp, LCs[i]]
+    }
+    N[regrow_id.pp,k,2,] <- 0
   } else {
     regrow_id.pp <- regrow_id <- NULL
   }
   altered_id.pp <- unique(c(cut_k$id.pp, regrow_id.pp))
-  # convert to Open
-  parcel.df[cut_k$id.pp, "Opn"] <- parcel.df[cut_k$id.pp, "Opn"] +
-    rowSums(parcel.df[cut_k$id.pp, forest.pp] * cut_k[,-1])
+  # convert cut forest percentages to Rgrw
+  parcel.df[cut_k$id.pp, "Rgrw"] <- rowSums(parcel.df[cut_k$id.pp, forest.pp] * 
+                                              cut_k[,-1])
   # reduce forest cover by specified proportions
   parcel.df[cut_k$id.pp, forest.pp] <- parcel.df[cut_k$id.pp, forest.pp] *
     (1 - cut_k[,-1])
@@ -204,36 +234,61 @@ for(k in 1:g.p$tmax) {
   # udpate SDD neighborhoods
   sdd.alter <- tibble(id.in=unique(unlist(sdd.ji[lc.df$id.in[altered_id]])),
                       id=lc.df$id[match(id.in, lc.df$id.in)])
-  sdd_new <- sdd_update_probs(lc.df, g.p, sdd.alter, sdd$i)
+  sdd_new <- sdd_update_probs(lc.df, g.p, sdd.alter, sdd$i, lc.col=4:10)
   sdd$i[,,1,sdd.alter$id.in] <- sdd_new$i
   sdd$sp[sdd.alter$id.in] <- sdd_new$sp
   # update carrying capacities
-  parcel.df$K_pp[altered_id.pp] <-
-    (as.matrix(parcel.df[altered_id.pp,LCs]) %*% g.p$K) %>%
-    multiply_by(parcel.df$Grid_Proportion[altered_id.pp]) %>%
-    c %>% ceiling
+  for(l in 1:g.p$n.lc) {
+    parcel.df[altered_id.pp, paste0("K_", LCs[l])] <- ceiling(
+      parcel.df[altered_id.pp,LCs[l]] * g.p$K[l] * 
+        parcel.df$Grid_Proportion[altered_id.pp])
+  }
+  parcel.df$K_pp[altered_id.pp] <- rowSums(
+    parcel.df[altered_id.pp, paste0("K_", LCs)])
+  # append current year harvests to cut.df
+  cut.df <- bind_rows(cut.df,
+                      data.frame(id.pp=cut_k$id.pp,
+                                 T_regrow=(1+beta*N[cut_k$id.pp,k,2,7]/
+                                             parcel.df$K_Rgrw[cut_k$id.pp])*L.0))
   
   
   ##---
   ## 3. Management plans are decided
   ##---
   # Economic decision model: which pixel-parcels perform which mgmt actions? 
-  N.k <- rowSums(N[,k,,7])  # buckthorn adult abundance
-  presence_pp <- which(N.k>0)  # id.pp with adult buckthorn
-  action_pp <- sapply(presence_pp,
-                      function(x) get.actions2(N_i=N.k[x],
-                                               K_i=parcel.df$K_pp[x],
-                                               PP_R=parcel.df$Wilderness_PP[x],
-                                               PP_T=parcel.df$Timber_PP[x],
-                                               TRT=trt.i$trt,
-                                               N_EFF=trt.i$N_eff,
-                                               T_EFF=trt.i$biomass_eff,
-                                               W=trt.i$wild_eff,
-                                               CHEM=trt.i$chem,
-                                               B=WTP_b,
-                                               COST=trt.i$cost))
-  act.mx[presence_pp,k] <- action_pp # store pixel-parcel decisions
-  mech_chem_id.pp <- presence_pp[action_pp != "N"] # id.pp that are treating
+  # N[id.pp (1:npp), year (1:tmax), LC (1:7), age (1:7)]
+  N.k.1 <- rowSums(N[,k,which(LCs %in% Mgmt1_LC),7])
+  N.k.2 <- N[,k,which(LCs %in% Mgmt2_LC),7]
+  presence_pp.1 <- which(N.k.1>0)  # id.pp with adult buckthorn
+  presence_pp.2 <- which(N.k.2>0)  # id.pp with adult buckthorn
+  action_pp.1 <- sapply(presence_pp.1,
+                        function(x) get.actions2(N_i=N.k.1[x],
+                                                 K_i=sum(parcel.df[x,paste0("K_", Mgmt1_LC)]),
+                                                 PP_R=parcel.df$Wilderness_PP[x],
+                                                 PP_T=parcel.df$Timber_PP[x],
+                                                 TRT=trt.i$trt,
+                                                 N_EFF=trt.i$N_eff,
+                                                 T_EFF=0,
+                                                 W=trt.i$wild_eff,
+                                                 CHEM=trt.i$chem,
+                                                 B=WTP_b,
+                                                 COST=trt.i$cost))
+  action_pp.2 <- sapply(presence_pp.2,
+                        function(x) get.actions2(N_i=N.k.2[x],
+                                                 K_i=sum(parcel.df[x,paste0("K_", Mgmt2_LC)]),
+                                                 PP_R=parcel.df$Wilderness_PP[x],
+                                                 PP_T=parcel.df$Timber_PP[x],
+                                                 TRT=trt.i$trt,
+                                                 N_EFF=trt.i$N_eff,
+                                                 T_EFF=trt.i$biomass_eff,
+                                                 W=trt.i$wild_eff,
+                                                 CHEM=trt.i$chem,
+                                                 B=WTP_b,
+                                                 COST=trt.i$cost))
+  act1.mx[presence_pp.1,k] <- action_pp.1 # store pixel-parcel decisions
+  act2.mx[presence_pp.2,k] <- action_pp.2 
+  mech_chem_id.pp <- unique(c(presence_pp.1[action_pp.1 != "N"], 
+                              presence_pp.2[action_pp.2 != "N"]))
 
   # Set control parameters, identify treated pixel-parcels
   control_par <- set_control_p(null_ctrl=F,
@@ -242,14 +297,14 @@ for(k in 1:g.p$tmax) {
   if(length(mech_chem_id.pp)>0) {
     # If treatment varies by land cover type, this needs to be a dataframe with
     # nLC + 1 columns (id, Trt.Opn, Trt.Oth, etc) and a row per pixel-parcel. 
-    # As an example, here all treatments are in Open and White Pine.
     mech_chem.i <- data.frame(id=mech_chem_id.pp,
-                              Trt.Opn=action_pp[action_pp != "N"],
+                              Trt.Opn=act1.mx[mech_chem_id.pp,k],
+                              Trt.Rgrw=act2.mx[mech_chem_id.pp,k],
                               Trt.Oth="N",
-                              Trt.Dec="N",
-                              Trt.Evg="N",
-                              Trt.WP=action_pp[action_pp != "N"],
-                              Trt.Mxd="N")
+                              Trt.Dec=act1.mx[mech_chem_id.pp,k],
+                              Trt.Evg=act1.mx[mech_chem_id.pp,k],
+                              Trt.WP=act1.mx[mech_chem_id.pp,k],
+                              Trt.Mxd=act1.mx[mech_chem_id.pp,k])
   } else {
     mech_chem.i <- NULL
   }
@@ -291,18 +346,27 @@ out.all <- left_join(lc.df, N.df, by="id.in") %>%
   gather(year, N, (ncol(lc.df)+1):ncol(.)) %>% 
   mutate(year=as.numeric(year),
          B=c(B))
-act.ls <- setNames(vector("list", 4), trt.i$trt)
+act1.ls <- act2.ls <- setNames(vector("list", 4), trt.i$trt)
 for(i in 1:length(trt.i$trt)) {
-  act.ls[[i]] <- map_dfr(setNames(1:ncell, 1:ncell), 
-                          ~colSums(act.mx[pp.ls[[.]],]==trt.i$trt[i])/
+  act1.ls[[i]] <- map_dfr(setNames(1:ncell, 1:ncell), 
+                          ~colSums(act1.mx[pp.ls[[.]],]==trt.i$trt[i])/
                            length(pp.ls[[.]])) %>%
     mutate(year=1:(g.p$tmax),
            action=trt.i$trt[i]) %>%
-    gather(id.in, prop, 1:ncell) %>%
+    gather(id.in, prop.1, 1:ncell) %>%
+    mutate(id.in=as.numeric(id.in))
+  act2.ls[[i]] <- map_dfr(setNames(1:ncell, 1:ncell), 
+                          ~colSums(act2.mx[pp.ls[[.]],]==trt.i$trt[i])/
+                            length(pp.ls[[.]])) %>%
+    mutate(year=1:(g.p$tmax),
+           action=trt.i$trt[i]) %>%
+    gather(id.in, prop.2, 1:ncell) %>%
     mutate(id.in=as.numeric(id.in))
 }
 out.actions <- full_join(filter(lc.df, inbd), 
-                         do.call("rbind", act.ls), by="id.in")
+                         do.call("rbind", act1.ls), by="id.in") 
+out.actions$prop.2 <- full_join(filter(lc.df, inbd), 
+                                do.call("rbind", act2.ls), by="id.in")$prop.2
 out.df <- lc.df %>%
   mutate(N.0=out.all$N[out.all$year==1],
          B.0=B[,1],
@@ -336,14 +400,26 @@ if(gifs) {
                       ggtitle("Seed abundance. Year {frame_time}"),
                     nframes=n_distinct(out.all$year), 
                     width=800, height=600, units="px", fps=10))
-  anim_save(paste0(plot.dir, "Parcel_decisions.gif"),
+  anim_save(paste0(plot.dir, "Parcel_decisions_1.gif"),
             animate(ggplot(out.actions, aes(lon, lat)) + 
-                      geom_tile(aes(fill=prop)) +
+                      geom_tile(aes(fill=prop.1)) +
                       scale_fill_viridis(name="Proportion of\npixel-parcels", 
                                          option="B") + 
                       transition_time(year) +
                       facet_wrap(~action) +
-                      ggtitle("Management decisions. Year {frame_time}"),
+                      ggtitle(paste("Management decisions for",
+                                    Mgmt1_LC, "Year {frame_time}")),
+                    nframes=n_distinct(out.all$year), 
+                    width=1100, height=1000, units="px", fps=10))
+  anim_save(paste0(plot.dir, "Parcel_decisions_2.gif"),
+            animate(ggplot(out.actions, aes(lon, lat)) + 
+                      geom_tile(aes(fill=prop.2)) +
+                      scale_fill_viridis(name="Proportion of\npixel-parcels", 
+                                         option="B") + 
+                      transition_time(year) +
+                      facet_wrap(~action) +
+                      ggtitle(paste("Management decisions for",
+                                    Mgmt2_LC, "Year {frame_time}")),
                     nframes=n_distinct(out.all$year), 
                     width=1100, height=1000, units="px", fps=10))
 }
