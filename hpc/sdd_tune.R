f1a44141b9b899337cee8b89bda996e4694fba7f
Sz-Tim <tim.szewczyk@gmail.com>
2018-11-19 17:47:31 -0700

    plots

--- hpc/sdd_tune.R
+++ hpc/sdd_tune.R
@@ -23,11 +23,12 @@ Packages <- c("gbPopMod", "tidyverse", "magrittr", "here", "doSNOW","fastmatch")
 suppressMessages(invisible(lapply(Packages, library, character.only=TRUE)))
 
 # set parameters
+plots <- FALSE
 res <- c("20ac", "9km2")[2]
 g.p <- set_g_p(tmax=96, lc.r=Inf, lc.c=Inf, N.p.t0=1, n.cores=4)
 par.ls <- set_sensitivity_pars(names(g.p)[c(15,16,18)], "gb", res)
 g.p$N.0 <- 10
-nSamp <- 1000
+nSamp <- 2500
 if(res=="9km2") {
   g.p$K <- c(3133908, 0, 462474, 462474, 462474, 462474)
   g.p$sdd.max <- 7
@@ -70,25 +71,38 @@ for(i in seq_along(f.nums)) {
 }
 
 out <- map_dfr(dir(paste0(out.dir, "pred"), full.names=T), read.csv)
+write_csv(out, paste0(out.dir, "dispersal_out.csv"))
 
-ggplot(out, aes(sdd.rate, pCorr, colour=n.ldd)) + 
-  geom_vline(xintercept=g.p$sdd.rate) + 
-  geom_point(alpha=0.8) + stat_smooth(method="loess", se=F) + 
-  facet_wrap(~Year) + scale_colour_viridis(option="B")
-ggplot(out, aes(sdd.max, pCorr)) + geom_boxplot(aes(group=sdd.max)) +
-  stat_smooth(method="loess") + facet_wrap(~Year)
-ggplot(out, aes(n.ldd, pCorr)) + geom_boxplot(aes(group=n.ldd)) +
-  stat_smooth(method="loess") + facet_wrap(~Year)
+if(plots) {
+  library(viridis)
+  ggplot(out, aes(sdd.rate, pCorr, colour=n.ldd)) + 
+    geom_vline(xintercept=g.p$sdd.rate) + 
+    geom_point(alpha=0.8) + stat_smooth(method="loess", se=F) + 
+    facet_wrap(~Year) + scale_colour_viridis(option="B")
+  ggplot(out, aes(sdd.max, pCorr)) + geom_boxplot(aes(group=sdd.max)) +
+    stat_smooth(method="loess") + facet_wrap(~Year)
+  ggplot(out, aes(n.ldd, pCorr)) + geom_boxplot(aes(group=n.ldd)) +
+    stat_smooth(method="loess") + facet_wrap(~Year)
+  
+  ggplot(filter(out, Year==2018), aes(sdd.rate, pOcc, colour=n.ldd, group=n.ldd)) + 
+    geom_point(alpha=0.8) + stat_smooth(method="loess", se=F) + 
+    scale_colour_viridis(option="B")
+  ggplot(filter(out, Year==2018), aes(sdd.max, pOcc, colour=n.ldd, group=n.ldd)) + 
+    geom_point(alpha=0.8) + stat_smooth(method="loess", se=F) + 
+    scale_colour_viridis(option="B")
+  ggplot(filter(out, Year==2018), aes(n.ldd, pOcc)) + geom_boxplot(aes(group=n.ldd)) +
+    stat_smooth(method="loess")
+}
 
 
 
-N.df <- map(dir(paste0(out.dir, "sims"), "N", full.names=T), 
-            ~data.frame(N=c(readRDS(.)), 
-                        Year=rep(1:g.p$tmax, each=ngrid))) %>%
-  do.call("rbind", .) %>%
-  mutate(id=rep(1:ngrid, times=length(dir(paste0(out.dir, "sims"), "N"))*g.p$tmax),
-         Occ=N>0) %>%
-  group_by(id, Year) %>%
-  summarise(pOcc=mean(Occ), mnN=mean(N))
+# N.df <- map(dir(paste0(out.dir, "sims"), "N", full.names=T), 
+#             ~data.frame(N=c(readRDS(.)), 
+#                         Year=rep(1:g.p$tmax, each=ngrid))) %>%
+#   do.call("rbind", .) %>%
+#   mutate(id=rep(1:ngrid, times=length(dir(paste0(out.dir, "sims"), "N"))*g.p$tmax),
+#          Occ=N>0) %>%
+#   group_by(id, Year) %>%
+#   summarise(pOcc=mean(Occ), mnN=mean(N))
 
 
