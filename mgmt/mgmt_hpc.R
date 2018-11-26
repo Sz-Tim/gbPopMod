4c03c9a55f50cb970c300f0842d41bc43f82d377
Sz-Tim <tim.szewczyk@gmail.com>
2018-11-26 16:14:50 -0700

    set for hpc with parallelized iterations

--- mgmt/mgmt_hpc.R
+++ mgmt/mgmt_hpc.R
@@ -4,9 +4,9 @@
 # cluster, and the number of cores to use can be specified with
 # set_g_p(n.cores=...). We explore four different broad plans:
 # 1) No management
-# 2) Current management plan (property-specific)
-# 3) Manual removal for populations  > 500 individuals/20-acres
-# 4) Manual removal + cover crop for populations > 500 individuals/20-acres
+# 2) Current stated management plan (property-specific)
+# 3) Current practiced management plan (property-specific)
+# 3) Manual removal for any populations > 500 individuals/20-acres
 
 # The buckthorn model functions are stored as an R package called gbPopMod
 # hosted on GitHub. Prior to publication, the repository is private. You can
@@ -24,7 +24,8 @@ suppressMessages(invisible(lapply(Packages, library, character.only=TRUE)))
 
 # set parameters
 res <- c("20ac", "9km2")[2]
-dem_par <- set_g_p(tmax=20)
+n.sim <- 10
+dem_par <- set_g_p(tmax=20, n.cores=4)
 if(res == "9km2") {
   dem_par$K <- c(3133908, 0, 462474, 462474, 462474, 462474)
   dem_par$sdd.max <- 7
@@ -33,7 +34,7 @@ if(res == "9km2") {
 
 # load landscape
 # load(paste0("data/USDA_", res, ".rda")) # loads landscape as lc.df
-lc.df <- read.csv("data/USDA_UNH_mgmt.csv")
+lc.df <- read.csv(paste0("data/USDA_", res, "_mgmt.csv"))
 ngrid <- nrow(lc.df)
 ncell <- sum(lc.df$inbd)
 
@@ -43,8 +44,8 @@ B_0 <- readRDS(paste0("data/inits/B_2018_", res, ".rds"))[lc.df$id.full]
 sdd <- sdd_set_probs(ncell, lc.df, dem_par, verbose=T)
 
 # management plans
-mgmt_plans <- dir("mgmt", "plan", full.names=T) %>% map(read.csv) %>%
-  setNames(str_remove(str_remove(dir("mgmt", "plan"), "plan_"), ".csv"))
+mgmt_plans <- dir("mgmt", "^p", full.names=T) %>% map(read.csv) %>%
+  setNames(str_remove(str_remove(dir("mgmt", "^p"), "p._"), ".csv"))
 mgmt <- setNames(vector("list", length(mgmt_plans)), names(mgmt_plans))
 lc.UNH <- filter(lc.df, in.UNH)
 for(m in seq_along(mgmt)) {
@@ -64,61 +65,70 @@ for(m in seq_along(mgmt)) {
                       trt.followup=mgmt_m$trt.followup)
   }
 } 
+
+# storage and initialization objects
 out.df.ls <- out.all.ls <- setNames(vector("list", 4), names(mgmt))
+N <- array(0, dim=c(ngrid, dem_par$tmax+1, 6, max(dem_par$m)))
+N[,1,,] <- N_0
+B <- matrix(0, nrow=ngrid, ncol=dem_par$tmax+1)
+B[,1] <- B_0
 
 for(m in seq_along(mgmt)) {
-  # storage objects
-  # N = abundances; dim=[cell, year, LC, age]
-  N <- array(0, dim=c(ngrid, dem_par$tmax+1, 6, max(dem_par$m)))
-  N[,1,,] <- N_0
-  # dem_par$m = age at maturity; varies by LC, so some are NA
-  # N[,,, dem_par$m] stores the number of buckthorn adults
-  # N[,,, 1:(dem_par$m-1)] stores the number of buckthorn juveniles 
-  
-  # B = seed bank; dim=[cell, year]
-  B <- matrix(0, nrow=ngrid, ncol=dem_par$tmax+1)
-  B[,1] <- B_0
-  
   # ids of all managed cells
   managed <- unique(c(mgmt[[m]]$manual.i$id, mgmt[[m]]$cover.i$id))
   
-  # iterate through years
-  for(k in 1:dem_par$tmax) {
-    
-    # decide which cells are managed this year
-    if(names(mgmt)[m] != "none" && 
-       any((k %% mgmt[[m]]$trt.int) <= mgmt[[m]]$trt.followup)) {  
-      managed_k <- managed[apply(N[managed,k,,], 1, sum) > mgmt[[m]]$thresh &
-                             (k %% mgmt[[m]]$trt.int) <= mgmt[[m]]$trt.followup]
-      manual_k <- mgmt[[m]]$manual.i[mgmt[[m]]$manual.i$id %in% managed_k,]
-      cover_k <- mgmt[[m]]$cover.i[mgmt[[m]]$cover.i$id %in% managed_k,]
-    } else {
-      manual_k <- NULL
-      cover_k <- NULL
-    }
-    
-    # buckthorn management, population growth, dispersal
-    out <- iterate_pop(ngrid, ncell, N[,k,,], B[,k], dem_par, lc.df[,-(15:18)], 
-                       sdd, mgmt[[m]]$ctrl_par, cover_k, manual_k)
-    
-    # update abundances
-    N[,k+1,,] <- out$N
-    B[,k+1] <- out$B
-    k <- k+1
-  }
+  # storage for simulation output
+  N_m <- B_m <- vector("list", n.sim)
   
-  # summarise
-  N.tot <- apply(N[,,,max(dem_par$m)], 1:2, sum) # sum adults across LCs
+  # iterate through simulations
+  p.c <- makeCluster(dem_par$n.cores); registerDoSNOW(p.c)
+  out_m <- foreach(s=1:n.sim, 
+                   .packages=c("gbPopMod", "tidyverse", "magrittr")) %dopar% {
+    # iterate through years
+    N_s <- N
+    B_s <- B
+    for(k in 1:dem_par$tmax) {
+      # decide which cells are managed this year
+      if(names(mgmt)[m] != "none" && 
+         any((k %% mgmt[[m]]$trt.int) <= mgmt[[m]]$trt.followup)) {  
+        managed_k <- managed[apply(N_s[managed,k,,], 1, sum) > mgmt[[m]]$thresh &
+                               (k %% mgmt[[m]]$trt.int) <= mgmt[[m]]$trt.followup]
+        manual_k <- mgmt[[m]]$manual.i[mgmt[[m]]$manual.i$id %in% managed_k,]
+        cover_k <- mgmt[[m]]$cover.i[mgmt[[m]]$cover.i$id %in% managed_k,]
+      } else {
+        manual_k <- NULL
+        cover_k <- NULL
+      }
+      
+      # buckthorn management, population growth, dispersal
+      out <- iterate_pop(ngrid, ncell, N_s[,k,,], B_s[,k], dem_par, 
+                         lc.df[,-(15:18)], sdd, mgmt[[m]]$ctrl_par, 
+                         cover_k, manual_k)
+      
+      # update abundances
+      N_s[,k+1,,] <- out$N
+      B_s[,k+1] <- out$B
+    }
+    # return abundances
+    list(N=N_s, B=B_s)
+  }
+  stopCluster(p.c)
+  N_m <- map(out_m, ~.$N)
+  B_m <- map(out_m, ~.$B)
+  
+  # summarise: means across simulations
+  N.tot <- Reduce('+', map(N_m, ~apply(.[,,,max(dem_par$m)], 1:2, sum)))/n.sim
+  B.tot <- Reduce('+', B_m)/n.sim
   N.df <- as.data.frame(N.tot); names(N.df) <- 1:ncol(N.df)
   out.all.ls[[m]] <- cbind(lc.df, N.df) %>%
     gather(year, N, (ncol(lc.df)+1):ncol(.)) %>% 
     mutate(year=as.numeric(year),
-           B=c(B))
+           B=c(B.tot))
   out.df.ls[[m]] <- lc.df %>%
     mutate(N.0=N.tot[,1],
-           B.0=B[,1],
+           B.0=B.tot[,1],
            N.final=N.tot[,dem_par$tmax+1],
-           B.final=B[,dem_par$tmax+1])
+           B.final=B.tot[,dem_par$tmax+1])
 }
 
 # plots
